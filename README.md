7825bf2672f79545dcc1ca8ff60bf628ad81a2e9
qiekn <qieknxxx@gmail.com>
2025-08-23 16:06:27 +0800

    feat: webassembly
    update run.sh
    update cmakelists
    update main loop (emscripten)

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
 # Quickstart
 
+## Build for Desktop
+
 ```bash
-git clone https://github.com/qiekn/raylib-quickstart.git
-cmake -B build && make -j$(nproc) -C build
+cmake -S . -B build && cmake --build build
 ./build/game
 ```
 
-`compile_commands.json` for neovim lsp config:
+## Build for WebAssembly
+
+Firstly read [Raylib Docs](https://github.com/raysan5/raylib/wiki/Working-for-Web-(HTML5))
+
+Install [emscripten toolchain](https://emscripten.org/docs/getting_started/downloads.html).
+If you are on macOS, you can install Emscripten using Homebrew: `brew install emscripten`
+
 
 ```bash
-cd path/to/your/project
-ln -s build/compile_commands.json ./compile_commands.json
-```
+mkdir build
+cd build
+cmake .. -DCMAKE_TOOLCHAIN_FILE=/Users/leoua7/Apps/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -DPLATFORM=Web
+make
--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,51 @@
 #include <raylib.h>
 
+#if defined(PLATFORM_WEB)
+#include <emscripten/emscripten.h>
+#endif
+
 const int kScreenWidth = 800;
 const int kSreenHeight = 450;
 
-int main() {
+void Loop();
+void Update();
+void Draw();
 
+int main() {
   SetTraceLogLevel(LOG_WARNING);
 
   InitWindow(kScreenWidth, kSreenHeight, "game");
 
   SetTargetFPS(60);
 
+#if defined(PLATFORM_WEB)
+  emscripten_set_main_loop(Loop, 0, 1);
+#else
+  SetTargetFPS(120);
   while (!WindowShouldClose()) {
-    // Update
-
-    // Draw
-    BeginDrawing();
-
-    ClearBackground(RAYWHITE);
-
-    DrawText("Congrats! You created your first window!", 190, 200, 20,
-             LIGHTGRAY);
-
-    EndDrawing();
+    Loop();
   }
+#endif
 
   CloseWindow();
-
   return 0;
+}
+
+void Loop() {
+  Update();
+  Draw();
+}
+
+void Update() {
+  // TODO
+}
+
+void Draw() {
+  BeginDrawing();
+
+  ClearBackground(RAYWHITE);
+
+  DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+  EndDrawing();
 }
