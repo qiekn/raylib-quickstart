84fccae5eb75ead32082fbc857ae37be4ae4fd00
qiekn <qiekndev@gmail.com>
2026-03-31 22:57:06 +0800

    deps: add raylib 5.5 as submodule

--- run.sh
+++ run.sh
@@ -6,21 +6,21 @@ PROJECT_NAME="game"
 WEBBUILD="build"
 BUILD_DIR="build"
 
-DEBUGGER="lldb"
+DEBUGGER="gdb -q"
 
 # Ensure build dir exists
 if [ ! -d "${BUILD_DIR}" ]; then
   echo "[INFO] ${BUILD_DIR} not found, mkdir ${BUILD_DIR}and running cmake..."
-  cmake -S . -B "${BUILD_DIR}" || exit 1
+  cmake -S . -B "${BUILD_DIR}" -G Ninja || exit 1
 fi
 
 if [ "$1" = "debug" ]; then
   ${DEBUGGER} "./${BUILD_DIR}/${PROJECT_NAME}"
 elif [ "$1" = "web" ]; then
-  make -j"$(nproc)" -C "${WEBBUILD}" && "./${WEBBUILD}/out/index.html"
+  cmake --build ${WEBBUILD} -j  && "./${WEBBUILD}/out/index.html"
 else
   # Default: build for desktop
-  make -j"$(nproc)" -C "${BUILD_DIR}" && "./${BUILD_DIR}/${PROJECT_NAME}"
+  cmake --build ${BUILD_DIR} -j  && "./${BUILD_DIR}/${PROJECT_NAME}"
 fi
 
 # vim: ft=sh ts=2 sw=2 et
